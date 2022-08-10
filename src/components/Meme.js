0a4fe78422f5c1c937914038278c40447e37d128
Marshal Olu <olmarsh.192@gmail.com>
2022-08-10 23:16:08 +0100

    added the function to display the random images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,16 @@
+import React from "react"
 import memesData from "../memesData.js";
 
 
 
 function Meme() {
 
+    const [memeImage, setMemeImage] = React.useState("")
+
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        setMemeImage(memesArray[randomNumber].url)
     }
 
     return (
@@ -29,6 +32,7 @@ function Meme() {
                     Get a new meme image 🖼
                     </button>
             </div>
+            <img src={memeImage} className="meme__image" alt="" />
         </main>
     )
 }
