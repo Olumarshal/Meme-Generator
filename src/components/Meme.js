ee763c74792bc19764c600cc33e7c4bbeace0354
Marshal Olu <olmarsh.192@gmail.com>
2022-08-15 12:35:06 +0100

    added the form text css

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,57 +1,44 @@
 /* eslint-disable no-unused-vars */
-import React from "react"
+import React from "react";
 import memesData from "../memesData.js";
 
-
-
 function Meme() {
-    const [meme, setMeme] = React.useState({
-        topText:"",
-        bottomText:"",
-        randomImage:"http://i.imgflip.com/1bij.jpg" 
-    })
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
-    const [memeImage, setMemeImage] = React.useState("")
+  const [memeImage, setMemeImage] = React.useState("");
 
-    function getMemeImage() {
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url =  memesArray[randomNumber].url
-        setMeme(prevMeme =>({
-            ...prevMeme,
-            randomImage: url
-        }))
-    }
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
 
-    return (
-        <main>
-            <div className="form">
-                <input
-                    type="text"
-                    placeholder="Top text"
-                    className="form__input" 
-                    />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form__input" 
-                    />
-                <button
-                    className="form__button"
-                    onClick={getMemeImage}
-                    >
-                    Get a new meme image 🖼
-                    </button>
-            </div>
-            <div className="meme">
-            <img src={meme.randomImage} className="meme__image" alt="" />
-            <h2 className="meme__text top">One does not simply</h2>
-            <h2 className="meme __text bottom">Walk into Mordor</h2>
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form__input" />
+        <input type="text" placeholder="Bottom text" className="form__input" />
+        <button className="form__button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <div className="meme">
+        <img src={meme.randomImage} className="meme__image" alt="" />
+        <h2 className="meme__text top">One does not simply</h2>
+        <h2 className="meme__text bottom">Walk into Mordor</h2>
+      </div>
+    </main>
+  );
 }
 
-export default Meme
+export default Meme;
